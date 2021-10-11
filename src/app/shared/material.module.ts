c73c8f4346ba7349abcecdeaa1ff2546afc5345d
nikola <nikola.nikkolic@gmail.com>
2021-10-11 09:24:15 +0200

    initial data

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
+
+@NgModule({
+    imports: [],
+    exports: [MatSliderModule]
+})
+
+export class MaterialModule {}
