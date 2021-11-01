0e0057bd6bd98fef6a5b5cb10f4396c7aca30505
nikola <nikola.nikkolic@gmail.com>
2021-11-01 10:12:56 +0100

    Added myRoutes interface

--- src/app/modules/users/components/users-list/users-list.component.ts
+++ src/app/modules/users/components/users-list/users-list.component.ts
@@ -59,7 +59,7 @@ export class UsersListComponent implements OnInit {
             this.getUsersFromServer();
           }
         );
-      } 
+      }
     });
   }
 
--- src/app/shared/models/route/my-route.ts
+++ src/app/shared/models/route/my-route.ts
@@ -1,4 +1,4 @@
-import { Route } from "@angular/router";
+import { Route } from '@angular/router';
 
 export interface MyRoute extends Route {
     icon?: string;
